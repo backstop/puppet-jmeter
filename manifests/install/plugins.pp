8f22c5e2a4517ecdaa92e46f3dd854ced344aba7
Kevin Wolf <kwolf72@gmail.com>
2014-03-02 15:30:19 -0600

    removing unnecessary check

--- manifests/install/plugins.pp
+++ manifests/install/plugins.pp
@@ -6,18 +6,16 @@ class jmeter::install::plugins {
     'package' => '/usr/share/jmeter/lib/ext',
   }
 
-  if $jmeter_plugins_install == True {
-    wget::fetch { 'plugins':
-      source       => "http://jmeter-plugins.org/downloads/file/JMeterPlugins-Standard-${::jmeter::params::plugins_version}.zip",
-      destinations => "/root/JMeterPlugins-Standard-${::jmeter::params::plugins_version}.zip",
-      notify       => Exec['install-jmeter-plugins'],
-    }
+  wget::fetch { 'plugins':
+    source       => "http://jmeter-plugins.org/downloads/file/JMeterPlugins-Standard-${::jmeter::params::plugins_version}.zip",
+    destinations => "/root/JMeterPlugins-Standard-${::jmeter::params::plugins_version}.zip",
+    notify       => Exec['install-jmeter-plugins'],
+  }
 
-    exec { 'install-jmeter-plugins':
-      command => "unzip -q -d JMeterPlugins JMeterPlugins-${::jmeter::params::plugins_version}.zip && mv JMeterPlugins/JMeterPlugins.jar ${dest}",
-      cwd     => '/root',
-      creates => "${dest}/JMeterPlugins-Standard.jar",
-      require => [Package['unzip']]
-    }
+  exec { 'install-jmeter-plugins':
+    command => "unzip -q -d JMeterPlugins JMeterPlugins-${::jmeter::params::plugins_version}.zip && mv JMeterPlugins/JMeterPlugins.jar ${dest}",
+    cwd     => '/root',
+    creates => "${dest}/JMeterPlugins-Standard.jar",
+    require => [Package['unzip']]
   }
 }
